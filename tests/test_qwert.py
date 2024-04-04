f59af4e9e0a25ee6bd2f14ae9f430c1b366634ab
daywalker90 <8257956+daywalker90@users.noreply.github.com>
2024-04-04 11:16:25 +0200

    test

---
+++ tests/test_qwert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+from pyln.testing.utils import only_one, sync_blockheight
+
+def test_private_channel_receive(node_factory, bitcoind):  # noqa: F811
+    l1, l2 = node_factory.get_nodes(
+        2
+    )
+    l1.fundwallet(10_000_000)
+    # setup 2 nodes with 1 private and 1 public channel
+    l1.rpc.connect(l2.info["id"], "localhost", l2.port)
+    scid_pub, _ = l1.fundchannel(l2, 1_000_000)
+    bitcoind.generate_block(1)
+    sync_blockheight(bitcoind, [l1, l2])
+    scid_priv, _ = l1.fundchannel(
+        l2, 1_000_000, announce_channel=False, push_msat=500_000_000
+    )
+
+    bitcoind.generate_block(6)
+    sync_blockheight(bitcoind, [l1, l2])
+
+    l1.wait_channel_active(scid_pub)
+    l1.wait_local_channel_active(scid_priv)
