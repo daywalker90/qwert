4d3489eda91ce227fea83b7e27f21000545427ed
daywalker90 <8257956+daywalker90@users.noreply.github.com>
2024-04-04 13:46:47 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{anyhow, Error};
+use cln_plugin::{Builder, Plugin};
+use serde_json::json;
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    std::env::set_var("CLN_PLUGIN_LOG", "cln_plugin=info,cln_rpc=info,debug");
+    let confplugin = match Builder::new(tokio::io::stdin(), tokio::io::stdout())
+        .rpcmethod("qwert", "test", qwert)
+        .configure()
+        .await?
+    {
+        Some(plugin) => plugin,
+        None => return Err(anyhow!("Error configuring the plugin!")),
+    };
+    if let Ok(plugin) = confplugin.start(()).await {
+        plugin.join().await
+    } else {
+        Err(anyhow!("Error starting the plugin!"))
+    }
+}
+pub async fn qwert(_p: Plugin<()>, _args: serde_json::Value) -> Result<serde_json::Value, Error> {
+    Ok(json!({ "version": format!("v{}",env!("CARGO_PKG_VERSION")) }))
 }
--- tests/test_qwert.py
+++ tests/test_qwert.py
@@ -2,23 +2,24 @@
 
 from pyln.testing.fixtures import *  # noqa: F403
 from pyln.testing.utils import only_one, sync_blockheight
+from util import get_plugin  # noqa: F401
 
-def test_private_channel_receive(node_factory, bitcoind):  # noqa: F811
+def test_private_channel_receive(node_factory, bitcoind, get_plugin):  # noqa: F811
     l1, l2 = node_factory.get_nodes(
-        2
+        2, opts={"plugin": get_plugin}
     )
     l1.fundwallet(10_000_000)
-    # setup 2 nodes with 1 private and 1 public channel
     l1.rpc.connect(l2.info["id"], "localhost", l2.port)
     scid_pub, _ = l1.fundchannel(l2, 1_000_000)
     bitcoind.generate_block(1)
     sync_blockheight(bitcoind, [l1, l2])
-    scid_priv, _ = l1.fundchannel(
-        l2, 1_000_000, announce_channel=False, push_msat=500_000_000
-    )
+    scid_pub2, _ = l1.fundchannel(l2, 1_000_000)
 
     bitcoind.generate_block(6)
     sync_blockheight(bitcoind, [l1, l2])
 
     l1.wait_channel_active(scid_pub)
-    l1.wait_local_channel_active(scid_priv)
+    l1.wait_channel_active(scid_pub2)
+
+    result = l1.rpc.call("qwert")
+    assert("version" in result)
