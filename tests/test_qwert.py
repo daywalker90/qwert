1da6ec0f0a421cf319328de0eb342daeae134e20
daywalker90 <8257956+daywalker90@users.noreply.github.com>
2024-04-05 12:22:08 +0200

    test

--- tests/test_qwert.py
+++ tests/test_qwert.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python
 
 from pyln.testing.fixtures import *  # noqa: F403
-from pyln.testing.utils import only_one, sync_blockheight
+from pyln.testing.utils import sync_blockheight
 from util import get_plugin  # noqa: F401
+
 
 def test_private_channel_receive(node_factory, bitcoind, get_plugin):  # noqa: F811
     l1, l2 = node_factory.get_nodes(
-        2, opts={"plugin": get_plugin}
+        2,
+        opts={"plugin": get_plugin, "log-level": "io"},
     )
     l1.fundwallet(10_000_000)
+    l2.fundwallet(10_000_000)
     l1.rpc.connect(l2.info["id"], "localhost", l2.port)
-    scid_pub, _ = l1.fundchannel(l2, 1_000_000)
-    
+    l1.rpc.fundchannel(l2.info["id"], 1_000_000, mindepth=1)
+    l2.rpc.fundchannel(l1.info["id"], 1_000_000, mindepth=1)
+
     bitcoind.generate_block(6)
     sync_blockheight(bitcoind, [l1, l2])
 
-    l1.wait_channel_active(scid_pub)
-
     result = l1.rpc.call("qwert")
-    assert("version" in result)
-    
+    assert "version" in result
